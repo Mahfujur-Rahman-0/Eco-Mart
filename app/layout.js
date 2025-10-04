fc7122fe7a188786a0872302c9ab3139eac13d4e
Mahfujur-Rahman-1 <mjeem64@example.com>
2025-10-04 12:15:24 +0600

    kl

--- app/layout.js
+++ app/layout.js
@@ -5,7 +5,7 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 
 import Header from "./components/Header";
-import Footer from "./components/footer";
+import Footer from "./components/Footer";
 import AppProvider from "./context/AppContext";
 import AOSProvider from "./components/AOSProvider";
 
